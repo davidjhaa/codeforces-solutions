e9c17d6131de07550ad1df0c8f3a3f57a9d6635e
davidjhaa4u <davidjhaa4u@gmail.com>
2023-04-08 23:57:50 +0530

    first commit

---
+++ Young_Physicist.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int x=0,y=0,z=0;
+    while(n--){
+        int x1,y1,z1;
+        cin >> x1 >> y1 >> z1;
+        x += x1;
+        y += y1;
+        z += z1;
+    }
+    if(x == 0 && y == 0 && z == 0){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+}
