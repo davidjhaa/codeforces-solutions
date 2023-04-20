131d4664b0b9095230d29b898d0169143075bf28
davidjhaa4u <davidjhaa4u@gmail.com>
2023-04-20 23:51:51 +0530

    ...

---
+++ 14.Arrival of the General.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    int smallest = 0;
+    int largest = 0;
+    for(int i = 0; i < n; i++){
+        if(arr[i] > arr[largest]){
+            largest = i;
+        }
+        if(arr[i] <= arr[smallest]){
+            smallest = i;
+        }
+    }
+    if(smallest == n-1 && largest == 0){
+        cout << 0;
+        return 0;
+    }
+    else{
+        int ans = 0;
+        if(smallest > largest){
+            ans += largest;
+            ans += (n-1) - smallest;
+            cout << ans << "\n";
+        }
+        else{
+            cout << ans-1 << "\n";
+        }
+    }
+    return 0;
+}
